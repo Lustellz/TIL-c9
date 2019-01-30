780053e7426d6fa96ec5ff43219014366e8a2b88
RG Gwon <lustellz@gmail.com>
2019-01-30 03:22:17 +0000

    D21 | 190130 AM | Django URL Names

---
+++ django/crud+/posts/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name='posts'
+
+urlpatterns = [
+    
+    # path('github/<str:username>/',views.github),
+    # path('naver/<str:q>/',views.naver),
+    
+    path('',views.index,name='list'),
+    path('create/',views.create, name='create'),
+    path('write/',views.new,name='new'),
+    path('<int:post_id>/',views.detail,name='detail'),
+    path('<int:post_id>/delete/',views.delete,name='delete'),
+    path('<int:post_id>/edit/',views.edit,name='edit'),
+    path('<int:post_id>/update/',views.update,name='update'),
+]
--- django/workshop190129/students/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display=('name','email','birthday','age',)
-
-# Register your models here.
-
-admin.site.register(Student, StudentAdmin)
