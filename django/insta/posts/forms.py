e84e811352e0f518f08a8f2c5182902bf83fe342
RG Gwon <lustellz@gmail.com>
2019-04-11 08:46:43 +0000

    190411 | Insta - Comment, Like, People

--- django/insta/posts/forms.py
+++ django/insta/posts/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['content', 'image',]
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(label = '', widget = forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter Comment.'}))
+    
+    class Meta:
+        model = Comment
+        fields = ['content',]
